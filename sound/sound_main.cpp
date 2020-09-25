b1a2f5fa4ec3ef72c3a9e8adaedf44ba8744595e
bqqbarbhg <samuli.1995@hotmail.com>
2020-09-26 00:07:40 +0300

    Process sounds to takes

--- sound/sound_main.cpp
+++ sound/sound_main.cpp
@@ -41,6 +41,13 @@ void failf(const char *fmt, ...)
 	exit(1);
 }
 
+const char zero_buf[64] = { 0 };
+
+static void write_pad(FILE *f, size_t num)
+{
+	fwrite(zero_buf, 1, num, f);
+}
+
 static void write_data(FILE *f, const void *data, size_t size)
 {
 	size_t num = fwrite(data, 1, size, f);
@@ -50,7 +57,7 @@ static void write_data(FILE *f, const void *data, size_t size)
 	}
 }
 
-void *compress_audio(spfile_section *section, const void *data, size_t size, int level, sp_compression_type compression_type)
+void *compress_section(spfile_section *section, uint32_t *file_offset, uint32_t *pad, const void *data, size_t size, int level, sp_compression_type compression_type, spfile_section_magic magic)
 {
 	size_t bound = sp_get_compression_bound(compression_type, size);
 	void *result = malloc(bound);
@@ -64,22 +71,59 @@ void *compress_audio(spfile_section *section, const void *data, size_t size, int
 		compressed_size = size;
 	}
 
-	section->magic = SPFILE_SECTION_AUDIO;
+	while (*file_offset % 16 != 0) {
+		++*pad;
+		++*file_offset;
+	}
+
+	section->magic = magic;
 	section->index = 0;
 	section->compressed_size = (uint32_t)compressed_size;
 	section->compression_type = compression_type;
 	section->uncompressed_size = (uint32_t)size;
-	section->offset = sizeof(spsound_header);
+	section->offset = *file_offset;
+	*file_offset += section->compressed_size;
 
 	return result;
 }
 
+struct take
+{
+	const char *input_file;
+	void *data;
+	size_t data_size;
+	spsound_format format;
+};
+
+#define MAX_TAKES 1024
+
+take takes[MAX_TAKES];
+spsound_take sp_takes[MAX_TAKES];
+
+void *audio_data = nullptr;
+size_t audio_size = 0;
+size_t audio_capacity = 0;
+
+void push_audio(const void *data, size_t size)
+{
+	if (audio_size + size > audio_capacity) {
+		audio_capacity *= 2;
+		if (audio_capacity < audio_size + size) {
+			audio_capacity = audio_size + size;
+		}
+		audio_data = realloc(audio_data, audio_capacity);
+	}
+
+	memcpy((char*)audio_data + audio_size, data, size);
+	audio_size += size;
+}
+
 int main(int argc, char **argv)
 {
-	const char *input_file = NULL;
 	const char *output_file = NULL;
 	bool show_help = false;
 	int level = 10;
+	uint32_t num_takes = 0;
 
 	// -- Parse arguments
 
@@ -93,7 +137,11 @@ int main(int argc, char **argv)
 			show_help = true;
 		} else if (left >= 1) {
 			if (!strcmp(arg, "-i") || !strcmp(arg, "--input")) {
-				input_file = argv[++argi];
+				if (num_takes >= MAX_TAKES) {
+					failf("Too many takes");
+				}
+				takes[num_takes].input_file = argv[++argi];
+				num_takes++;
 			} else if (!strcmp(arg, "-o") || !strcmp(arg, "--output")) {
 				output_file = argv[++argi];
 			} else if (!strcmp(arg, "-l") || !strcmp(arg, "--level")) {
@@ -116,121 +164,125 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	FILE *f = fopen(input_file, "rb");
-	if (!f) failf("Failed to open input file: %s", input_file);
+	for (uint32_t i = 0; i < num_takes; i++) {
+		take *t = &takes[i];
 
-	fseek(f, 0, SEEK_END);
-	size_t file_size = ftell(f);
-	fseek(f, 0, SEEK_SET);
+		FILE *f = fopen(t->input_file, "rb");
+		if (!f) failf("Failed to open input file: %s", t->input_file);
 
-	void *file_data = malloc(file_size);
-	size_t num_read = fread(file_data, 1, file_size, f);
-	if (num_read != file_size) failf("Failed to read input file: %s", input_file);
+		fseek(f, 0, SEEK_END);
+		size_t file_size = ftell(f);
+		fseek(f, 0, SEEK_SET);
 
-	fclose(f);
+		void *file_data = malloc(file_size);
+		size_t num_read = fread(file_data, 1, file_size, f);
+		if (num_read != file_size) failf("Failed to read input file: %s", t->input_file);
+		fclose(f);
 
-	if (file_size >= 4 && !memcmp(file_data, "RIFF", 4)) {
-		// WAV file
-		if (file_size < sizeof(wav_header)) failf("Invalid WAVE file");
-		wav_header *wav = (wav_header*)file_data; 
+		if (file_size >= 4 && !memcmp(file_data, "RIFF", 4)) {
+			// WAV file
+			if (file_size < sizeof(wav_header)) failf("Invalid WAVE file");
+			wav_header *wav = (wav_header*)file_data; 
 
-		if (memcmp(wav->format, "WAVE", 4) != 0) failf("wav: Bad Format: %.4s", wav->format);
-		if (memcmp(wav->fmt_id, "fmt ", 4) != 0) failf("wav: Bad fmt chunk ID: %.4s", wav->fmt_id);
-		if (wav->fmt_size != 16) failf("wav: Bad fmt chunk size: %u", wav->fmt_size);
-		if (wav->audio_format != 1) failf("wav: Bad audio format: %u", wav->audio_format);
-		if (wav->num_channels < 1) failf("wav: Bad number of channels: %u", wav->num_channels);
-		if (wav->bits_per_sample != 8 && wav->bits_per_sample != 16 && wav->bits_per_sample != 24 && wav->bits_per_sample != 32) {
-			failf("wav: Bad number of channels: %u", wav->num_channels);
-		}
-		if (memcmp(wav->data_id, "data", 4) != 0) failf("wav: Bad data chunk ID: %.4s", wav->fmt_id);
-		uint32_t num_samples = wav->data_size / (wav->num_channels * (wav->bits_per_sample / 8));
-		uint32_t num_channels = wav->num_channels <= 2 ? wav->num_channels : 2;
-		if (file_size < sizeof(wav_header) + wav->data_size) failf("wav: Truncated file");
-
-		uint32_t stride = wav->num_channels*(wav->bits_per_sample/8);
-
-		const char *src = (const char*)file_data + sizeof(wav_header);
-		int16_t *pcm_data = (int16_t*)malloc(num_samples * num_channels * sizeof(int16_t));
-		int16_t *dst = pcm_data;
-
-		for (uint32_t i = 0; i < num_samples; i++) {
-			for (uint32_t j = 0; j < num_channels; j++) {
-				const char *s = src + i*stride + j*(wav->bits_per_sample/8);
-				int16_t d;
-				switch (wav->bits_per_sample / 8) {
-				case 1: d = (int16_t)((int32_t)*(uint8_t*)s * 0x101 - 32768); break;
-				case 2: d = *(int16_t*)s; break;
-				case 3: d = *(int16_t*)(s + 1); break;
-				case 4: d = *(int16_t*)(s + 2); break;
-				}
-				dst[j] = d;
+			if (memcmp(wav->format, "WAVE", 4) != 0) failf("wav: Bad Format: %.4s", wav->format);
+			if (memcmp(wav->fmt_id, "fmt ", 4) != 0) failf("wav: Bad fmt chunk ID: %.4s", wav->fmt_id);
+			if (wav->fmt_size != 16) failf("wav: Bad fmt chunk size: %u", wav->fmt_size);
+			if (wav->audio_format != 1) failf("wav: Bad audio format: %u", wav->audio_format);
+			if (wav->num_channels < 1) failf("wav: Bad number of channels: %u", wav->num_channels);
+			if (wav->bits_per_sample != 8 && wav->bits_per_sample != 16 && wav->bits_per_sample != 24 && wav->bits_per_sample != 32) {
+				failf("wav: Bad number of channels: %u", wav->num_channels);
 			}
-			dst += num_channels;
+			if (memcmp(wav->data_id, "data", 4) != 0) failf("wav: Bad data chunk ID: %.4s", wav->fmt_id);
+			uint32_t num_samples = wav->data_size / (wav->num_channels * (wav->bits_per_sample / 8));
+			uint32_t num_channels = wav->num_channels <= 2 ? wav->num_channels : 2;
+			if (file_size < sizeof(wav_header) + wav->data_size) failf("wav: Truncated file");
+
+			uint32_t stride = wav->num_channels*(wav->bits_per_sample/8);
+
+			const char *src = (const char*)file_data + sizeof(wav_header);
+			int16_t *pcm_data = (int16_t*)malloc(num_samples * num_channels * sizeof(int16_t));
+			int16_t *dst = pcm_data;
+
+			for (uint32_t i = 0; i < num_samples; i++) {
+				for (uint32_t j = 0; j < num_channels; j++) {
+					const char *s = src + i*stride + j*(wav->bits_per_sample/8);
+					int16_t d;
+					switch (wav->bits_per_sample / 8) {
+					case 1: d = (int16_t)((int32_t)*(uint8_t*)s * 0x101 - 32768); break;
+					case 2: d = *(int16_t*)s; break;
+					case 3: d = *(int16_t*)(s + 1); break;
+					case 4: d = *(int16_t*)(s + 2); break;
+					}
+					dst[j] = d;
+				}
+				dst += num_channels;
+			}
+
+			spsound_take *spt = &sp_takes[i];
+			spt->format = SPSOUND_FORMAT_PCM16;
+			spt->file_offset = (uint32_t)audio_size;
+			spt->file_size = num_samples * num_channels * sizeof(int16_t);
+			spt->length_in_samples = num_samples;
+			spt->length_in_seconds = (float)num_samples / (float)wav->sample_rate;
+			spt->num_channels = num_channels;
+			spt->sample_rate = wav->sample_rate;
+			spt->temp_memory_required = 0;
+
+			push_audio(pcm_data, spt->file_size);
+
+		} else if (file_size >= 4 && !memcmp(file_data, "OggS", 4)) {
+
+			int error;
+			stb_vorbis *v = stb_vorbis_open_memory((const unsigned char*)file_data, (int)file_size, &error, NULL);
+			if (error) failf("Failed to read Vorbis file: %d", error);
+
+			stb_vorbis_info info = stb_vorbis_get_info(v);
+
+			spsound_take *spt = &sp_takes[i];
+			spt->format = SPSOUND_FORMAT_VORBIS;
+			spt->file_offset = (uint32_t)audio_size;
+			spt->file_size = (uint32_t)file_size;
+			spt->length_in_samples = stb_vorbis_stream_length_in_samples(v);
+			spt->length_in_seconds = stb_vorbis_stream_length_in_seconds(v);
+			spt->num_channels = (uint32_t)info.channels;
+			spt->sample_rate = info.sample_rate;
+			spt->temp_memory_required = info.temp_memory_required + info.setup_temp_memory_required + info.setup_memory_required;
+
+			push_audio(file_data, (uint32_t)file_size);
+
+		} else {
+			failf("Unsupported file type");
 		}
 
-		int64_t hash = 0;
-		for (uint32_t i = 0; i < num_samples * num_channels; i++) {
-			hash = hash * 13 + pcm_data[i];
-		}
-
-		spsound_header header;
-		header.header.magic = SPFILE_HEADER_SPSOUND;
-		header.header.version = 1;
-		header.header.header_info_size = sizeof(spsound_info);
-		header.header.num_sections = 1;
-		header.info.format = SPSOUND_FORMAT_PCM16;
-		header.info.length_in_samples = num_samples;
-		header.info.length_in_seconds = (float)num_samples / (float)wav->sample_rate;
-		header.info.num_channels = num_channels;
-		header.info.sample_rate = wav->sample_rate;
-		header.info.temp_memory_required = 0;
-
-		void *data = compress_audio(&header.s_audio, pcm_data, num_samples*num_channels*sizeof(int16_t), level, SP_COMPRESSION_ZSTD);
-		free(pcm_data);
-
-		FILE *f = fopen(output_file, "wb");
-		if (!f) failf("Failed to open output file: %s", output_file);
-
-		write_data(f, &header, sizeof(header));
-		write_data(f, data, header.s_audio.compressed_size);
-
-		fclose(f);
-
-	} else if (file_size >= 4 && !memcmp(file_data, "OggS", 4)) {
-
-		int error;
-		stb_vorbis *v = stb_vorbis_open_memory((const unsigned char*)file_data, (int)file_size, &error, NULL);
-		if (error) failf("Failed to read Vorbis file: %d", error);
-
-		stb_vorbis_info info = stb_vorbis_get_info(v);
-
-		spsound_header header;
-		header.header.magic = SPFILE_HEADER_SPSOUND;
-		header.header.version = 1;
-		header.header.header_info_size = sizeof(spsound_info);
-		header.header.num_sections = 1;
-		header.info.format = SPSOUND_FORMAT_VORBIS;
-		header.info.length_in_samples = stb_vorbis_stream_length_in_samples(v);
-		header.info.length_in_seconds = stb_vorbis_stream_length_in_seconds(v);
-		header.info.num_channels = (uint32_t)info.channels;
-		header.info.sample_rate = info.sample_rate;
-		header.info.temp_memory_required = info.temp_memory_required + info.setup_temp_memory_required + info.setup_memory_required;
-
-		void *data = compress_audio(&header.s_audio, file_data, file_size, level, SP_COMPRESSION_NONE);
-
-		FILE *f = fopen(output_file, "wb");
-		if (!f) failf("Failed to open output file: %s", output_file);
-
-		write_data(f, &header, sizeof(header));
-		write_data(f, data, header.s_audio.compressed_size);
-
-		fclose(f);
-
-	} else {
-		failf("Unsupported file type");
+		free(file_data);
 	}
 
-	free(file_data);
+	spsound_header header;
+	header.header.magic = SPFILE_HEADER_SPSOUND;
+	header.header.version = 1;
+	header.header.header_info_size = sizeof(spsound_info);
+	header.header.num_sections = 2;
+	header.info.num_takes = num_takes;
+	header.info.temp = 0;
+
+	uint32_t file_offset = sizeof(spsound_header);
+	uint32_t take_pad = 0, audio_pad = 0;
+
+	void *take_comp = compress_section(&header.s_takes, &file_offset, &take_pad, sp_takes, num_takes * sizeof(spsound_take), level, SP_COMPRESSION_ZSTD, SPFILE_SECTION_TAKES);
+	void *audio_comp = compress_section(&header.s_audio, &file_offset, &audio_pad, audio_data, audio_size, level, SP_COMPRESSION_ZSTD, SPFILE_SECTION_TAKES);
+
+	FILE *f = fopen(output_file, "wb");
+	if (!f) failf("Failed to open output file: %s", output_file);
+
+	write_data(f, &header, sizeof(header));
+
+	write_pad(f, take_pad);
+	write_data(f, take_comp, header.s_takes.compressed_size);
+
+	write_pad(f, audio_pad);
+	write_data(f, audio_comp, header.s_audio.compressed_size);
+
+	fclose(f);
 
 	return 0;
 }
